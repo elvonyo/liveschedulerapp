214fb1bae42a035e1cbe842539ce02c7e04476da
elvonyo <tavonlg85@gmail.com>
2026-04-28 08:35:43 -0400

    your update

---
+++ sw.js
@@ -0,0 +1,26 @@
+self.addEventListener("push", function(event) {
+  var data = event.data ? event.data.json() : {};
+  event.waitUntil(
+    self.registration.showNotification(data.title || "LiveSupport Scheduler", {
+      body:  data.body  || "Someone just went live!",
+      icon:  "/icon-192.png",
+      badge: "/badge-72.png",
+      data:  { url: data.url || "/" },
+    })
+  );
+});
+
+self.addEventListener("notificationclick", function(event) {
+  event.notification.close();
+  event.waitUntil(
+    clients.matchAll({ type: "window" }).then(function(windowClients) {
+      var url = event.notification.data ? event.notification.data.url : "/";
+      for (var i = 0; i < windowClients.length; i++) {
+        if (windowClients[i].url === url && "focus" in windowClients[i]) {
+          return windowClients[i].focus();
+        }
+      }
+      if (clients.openWindow) return clients.openWindow(url);
+    })
+  );
+});
