e58d39b58b0484652daff5ef090a86361f05583f
elvonyo <tavonlg85@gmail.com>
2026-04-30 19:16:08 -0400

    fix: sw.js cache bust and webhook restore

--- public/sw.js
+++ public/sw.js
@@ -26,3 +26,4 @@ self.addEventListener("notificationclick", function(event) {
 });
 // updated Thu Apr 30 19:04:48 EDT 2026
 // reconnected Thu Apr 30 19:10:12 EDT 2026
+// reconnected Thu Apr 30 19:16:08 EDT 2026
